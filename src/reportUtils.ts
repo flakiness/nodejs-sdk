c83213bbf1a95dfd8149541be49f2903717fda14
Andrey Lushnikov <aslushnikov@gmail.com>
2025-12-26 12:01:21 -0800

    chore: fix reportUtils exports
    Since we don't bundle any more, we have to annotate type exports
    explicitly.

--- src/reportUtils.ts
+++ src/reportUtils.ts
@@ -2,6 +2,13 @@ export { createEnvironment } from './createEnvironment.js';
 export { createTestStepSnippetsInplace } from './createTestStepSnippets.js';
 export { normalizeReport } from './normalizeReport.js';
 export { stripAnsi } from './stripAnsi.js';
-export { Attachment, createDataAttachment, createFileAttachment, DataAttachment, FileAttachment } from './uploadReport.js';
+export {
+  createDataAttachment,
+  createFileAttachment
+} from './uploadReport.js';
+export type {
+  Attachment, DataAttachment,
+  FileAttachment
+} from './uploadReport.js';
 export { visitTests } from './visitTests.js';
 
